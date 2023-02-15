fa668de792285d15038bce85d0b933efe1c33006
Olga <pipchenkova@mail.ru>
2023-02-15 19:14:58 +0300

    correction

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿//                 Задача: Написать программу, которая из имеющегося 
-//                 массива строк формирует новый массив из строк, 
-//                 длина которых меньше, либо равна 3 символам. 
+﻿//                 Задача: Написать программу, которая из имеющегося
+//                 массива строк формирует новый массив из строк,
+//                 длина которых меньше, либо равна 3 символам.
 //                 Первоначальный массив можно ввести с клавиатуры,
 //                  либо задать на старте выполнения алгоритма.
-//                   При решении не рекомендуется пользоваться коллекциями, 
+//                   При решении не рекомендуется пользоваться коллекциями,
 //                   лучше обойтись исключительно массивами.
 
 // Примеры:
@@ -14,36 +14,35 @@
 
 // string[] array1 = new string[] {"Russia", "Denmark", "Kazan"};
 // string[] array1 = new string[] {"1234", "1567", "-2", "computer science"};
-string[] array1 = new string[] {"Hello", "2", "world", ":-)","rtgnsk","67y"," "};
+string[] array1 = new string[] { "Hello", "2", "world", ":-)", "rtgnsk", "67y", " " };
 
-//string[] array2 = new string[array1.Length];
-int SecondArraySize (string[] array1)
+int SecondArraySize(string[] array1)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        count++;
+            count++;
         }
     }
     return count;
 }
 
- string [] CreateArray(int size)
-     {
-     return new string[size];
-    }
-void Final(string[]array,string[] result)
-{   
+string[] CreateArray(int size)
+{
+    return new string[size];
+}
+void Final(string[] array, string[] result)
+{
     int size = array.Length;
     int pos = 0;
-    for (int i = 0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
-        if (array[i].Length <= 3 )
+        if (array[i].Length <= 3)
         {
-           result[pos]= array[i];
-           pos++;
+            result[pos] = array[i];
+            pos++;
         }
     }
 }
@@ -55,5 +54,5 @@ void Print(string[] Array)
 
 int size = SecondArraySize(array1);
 string[] result = CreateArray(size);
-Final(array1,result);
+Final(array1, result);
 Print(result);
